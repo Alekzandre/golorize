0c646109a38fab0a109926fe577242c3d7e1b1d3
alekzandre carayon <jacob@alekzandres-iMac.local>
2015-07-13 00:06:29 +0200

    add color.go golorize.go v1

---
+++ color.go
@@ -0,0 +1,51 @@
+package golorize
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+)
+
+func (g Golorize) color(s string, options ...string) {
+
+	var buffer bytes.Buffer
+	opNum, err := optionsSize(options)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	if opNum == 0 {
+		buffer.WriteString(START)
+		buffer.WriteString(NORMAL)
+		buffer.WriteString(F_WHITE)
+		buffer.WriteString(LIMIT)
+		buffer.WriteString(s)
+		buffer.WriteString(END)
+		fmt.Println(buffer.String())
+	} else if opNum == 1 {
+		buffer.WriteString(START)
+		buffer.WriteString(string(options[0]))
+		buffer.WriteString(F_WHITE)
+		buffer.WriteString(LIMIT)
+		buffer.WriteString(s)
+		buffer.WriteString(END)
+		fmt.Println(buffer.String())
+	} else if opNum == 2 {
+		buffer.WriteString(START)
+		buffer.WriteString(string(options[0]))
+		buffer.WriteString(options[1])
+		buffer.WriteString(LIMIT)
+		buffer.WriteString(s)
+		buffer.WriteString(END)
+		fmt.Println(buffer.String())
+	} else {
+		buffer.WriteString(START)
+		buffer.WriteString(string(options[0]))
+		buffer.WriteString(options[1])
+		buffer.WriteString(options[2])
+		buffer.WriteString(LIMIT)
+		buffer.WriteString(s)
+		buffer.WriteString(END)
+		fmt.Println(buffer.String())
+	}
+}
---
+++ golorize.go
@@ -0,0 +1,67 @@
+package golorize
+
+import (
+	"errors"
+)
+
+// Basic attributes
+
+const (
+	START = "\033["
+	END   = "\033[0m"
+	LIMIT = "m"
+)
+
+// Basic streams (default: Stdout)
+
+// Basic options
+
+const (
+	NORMAL     = "0;"
+	BOLD       = "1;"
+	UNDERLINED = "4;"
+	BLINKING   = "5;"
+	REVERSE    = "7;"
+)
+
+// Basic foreground color
+
+const (
+	F_BLACK   = "30"
+	F_RED     = "31"
+	F_GREEN   = "32"
+	F_YELLOW  = "33"
+	F_BLUE    = "34"
+	F_MAGENTA = "35"
+	F_CYAN    = "36"
+	F_WHITE   = "37"
+)
+
+// Basic background color
+
+const (
+	B_BLACK   = ";40"
+	B_RED     = ";41"
+	B_GREEN   = ";42"
+	B_YELLOW  = ";43"
+	B_BLUE    = ";44"
+	B_MAGENTA = ";45"
+	B_CYAN    = ";46"
+	B_WHITE   = ";47"
+)
+
+type Golorize struct{}
+
+// Calculate the number of options must be in range 0-3
+
+func optionsSize(options []string) (int, error) {
+	if len(options) > 3 {
+		err := errors.New("Too many arguments.")
+		return 0, err
+	}
+	return len(options), nil
+}
+
+func newGolorize() *Golorize {
+	return &Golorize{}
+}
